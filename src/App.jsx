e90159f2aa7dba6ecd5a6da6654612937583115a
ArielGon <arielgon2206@gmail.com>
2024-05-13 20:46:55 -0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
-import './App.css'
+import Header from "./components/Header"
+//import './App.css'
 
 function App() {
   
 
   return (
-    <div className='bg-red-500'>
-      hola
+    <div>
+      <Header/>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+
+export default function Header(){
+    return(
+        <div>
+            <nav>
+                <a href=""><img src="" alt="" width="28" /></a>
+                <div id="menu"> 
+                    <ul>
+                        <li><a href=""> Quien Soy </a></li>
+                        <li><a href=""></a> Experiencia </li>
+                        <li><a href=""></a> Proyectos </li>
+                        <li><a href=""></a> Contactame </li>
+                    </ul>
+
+                    <button id="darkButton">
+                        <i class="bi bi-moon-stars-fill"></i>
+                    </button>
+
+                    <button id="menu-button"></button>
+                </div>
+            </nav>
+        </div>
+    )
+}
