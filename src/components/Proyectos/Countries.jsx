976feafd3877ca600b4d64b7f39f9cb3fdffffbd
ArielGnz <arielgon2206@gmail.com>
2024-05-28 10:26:02 -0300

    estilos

--- src/components/Proyectos/Countries.jsx
+++ src/components/Proyectos/Countries.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 import countries from '../../assets/countries.jpg';
 
 export default function Countries() {
+
+  const countriesWeb = () =>{
+    const url = "https://pi-countries-nine-psi.vercel.app/";
+    window.open(url, '_blank');
+}
+ 
   return (
     <>
         <div className='m-5 w-[90%] h-[300px] lg:my-10 lg:w-[75%] lg:h-[100%] lg:content-center'>
@@ -12,8 +18,11 @@ export default function Countries() {
             <h1 className='text-2xl text-sky-500 text-center font-bold tracking-widest pb-6'> Countries </h1>
             <p className='text-white text-xl lg:w-[90%] lg:m-auto '>El proyecto de countries es una página web donde se pueden visualizar todos los países en su home Page,
              además se puede hacer una búsqueda por distintos tipos de filtros y también poder ordenar de manera alfabética o por cantidad de población.
-             También hay una sección donde a través de un formulario se pueden crear actividades y asignarla a los países. Podes visitar la pagina haciendo click aqui 
-             <button className='mx-4 p-1 rounded-md transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300'> Ver Pagina </button></p>
+             También hay una sección donde a través de un formulario se pueden crear actividades y asignarla a los países. Podes visitar la pagina haciendo click aqui.. 
+             <button className='mx-1 p-1 rounded-md transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300'
+                     onClick={countriesWeb}>
+               Ver Pagina 
+               </button></p>
         </div>
         
     </>
