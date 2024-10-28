211f8cbfd798a3eb11d2ee0a4bb131012cc8c45f
ArielGnz <arielgon2206@gmail.com>
2024-10-28 11:03:07 -0300

    actualizando

--- src/components/Proyectos/Uuid-store.jsx
+++ src/components/Proyectos/Uuid-store.jsx
@@ -15,8 +15,10 @@ const UuidStore = () => {
             <div className='text-white lg:h-[300px] w-[80%] m-auto lg:w-[80%] '>
                 <h1 className='text-2xl text-sky-500 text-center font-bold tracking-widest pb-6'> Uuid-store </h1>
                 <p className='text-white text-xl lg:w-[90%] lg:m-auto '>Proyecto final del bootcamp junto con otros 6 compañeros donde realizamos una e-commerce de zapatillas.</p>
+                
+                {/* <span className='text-white text-xl lg:w-[90%] lg:m-auto '>https://github.com/Agus-Albarracin/uuid-store</span> */}
+
                 <button onClick={github} className='pr-2 text-4xl text-white hover:text-sky-600'><i className='bi bi-github px-2'></i></button>
-                <span className='text-white text-xl lg:w-[90%] lg:m-auto '>https://github.com/Agus-Albarracin/uuid-store</span>
             </div>
         </>
     )
